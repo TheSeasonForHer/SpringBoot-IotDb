1a3663a8e783263857ca9a7b8bfd53cf181ba9c1
她说过的四季 <2318708003@qq.com>
2024-08-10 23:31:18 +0800

    1

--- src/main/java/com/iotdb/utils/TSDataTypeUtil.java
+++ src/main/java/com/iotdb/utils/TSDataTypeUtil.java
@@ -2,8 +2,12 @@ package com.iotdb.utils;
 
 
 import cn.hutool.core.date.DateTime;
+import com.iotdb.exception.ServiceException;
 import org.apache.tsfile.enums.TSDataType;
 import org.apache.tsfile.read.common.Field;
+
+import static com.iotdb.enums.StatusCodeEnum.VALID_ERROR;
+
 /**
  * @author tjb
  * @date 2024/8/2
@@ -33,7 +37,7 @@ public class TSDataTypeUtil {
             case "DATE":
                 return TSDataType.DATE;
             default:
-                throw new IllegalArgumentException("Invalid input type: " + type);
+                throw new ServiceException(VALID_ERROR.getCode(),"Invalid input type: " + type);
         }
     }
     public static Object getValueByFiled(Field field) {
@@ -41,70 +45,70 @@ public class TSDataTypeUtil {
         byte type = dataType.getType();
         switch (type) {
             case 0:
-                //BOOLEAN
+                // BOOLEAN
                 return field.getBoolV();
             case 1:
-                //INT32;
+                // INT32;
                 return field.getIntV();
             case 2:
-                //INT64;
+                // INT64;
                 return field.getLongV();
             case 3:
-//                return FLOAT;
+                // return FLOAT;
                 return field.getFloatV();
             case 4:
-//                return DOUBLE;
+                // return DOUBLE;
                 return field.getDoubleV();
             case 5:
-//                return TEXT;
+                // return TEXT;
                 return field.getStringValue();
             case 8:
-//                return TIMESTAMP;
+                // return TIMESTAMP;
                 return field.getDataType();
             case 9:
-//                return DATE;
+                // return DATE;
                 return field.getDateV();
             case 10:
-//                return BLOB;
+                // return BLOB;
                 return field.getBinaryV();
             case 11:
-//                return STRING;
+                // return STRING;
                 return field.getStringValue();
             default:
-                throw new IllegalArgumentException("Invalid input: " + type);
+                throw new ServiceException(VALID_ERROR.getCode(),"Invalid input type: " + type);
         }
     }
     public static Object getValueByData(int dataType,String data) {
         switch (dataType) {
             case 0:
-                //BOOLEAN
+                // BOOLEAN
                 return Boolean.parseBoolean(data);
             case 1:
-                //INT32;
+                // INT32;
                 return Integer.parseInt(data);
             case 2:
-                //INT64;
+                // INT64;
                 return Long.parseLong(data);
             case 3:
-//                return FLOAT;
+                // return FLOAT;
                 return Float.parseFloat(data);
             case 4:
-//                return DOUBLE;
+                // return DOUBLE;
                 return Double.parseDouble(data);
             case 5:
-//                return TEXT;
+                // return TEXT;
                 return data;
             case 8:
-//                return TIMESTAMP;
+                // return TIMESTAMP;
                 return Long.parseLong(data);
             case 9:
-//                return DATE;
+                // return DATE;
                 return DateTime.of(Long.parseLong(data));
             case 10:
-//                return BLOB;
+                // return BLOB;
                 return data;
             case 11:
-//                return STRING;
+                // return STRING;
                 return data;
             default:
                 throw new IllegalArgumentException("Invalid input: " + dataType);
