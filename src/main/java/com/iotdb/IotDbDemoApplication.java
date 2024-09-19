e93ff365293901bc714702bbfd3534526d88c6cb
Utopia <jinbiao.tang@timecho.com>
2024-09-19 14:01:32 +0800

    delete the aspect in IotDbDemoApplication

--- src/main/java/com/iotdb/IotDbDemoApplication.java
+++ src/main/java/com/iotdb/IotDbDemoApplication.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAspectJAutoProxy
 @EnableAsync
 public class IotDbDemoApplication {
 
